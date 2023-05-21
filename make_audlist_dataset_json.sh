cf6b05a96b167ffd9d35af6aa63f012b3993e7c4
Devin Markell <dmarkell@fb.com>
2023-05-21 07:31:30 -0700

    Small cleanups

--- make_audlist_dataset_json.sh
+++ make_audlist_dataset_json.sh
@@ -4,9 +4,9 @@ TD_1="/tmp/audlist_entitylist_ints.json"
 TD_2="/tmp/audlist_entities.json"
 
 cat "$1" |
-python $ANALYTICS_DIR"tsv_to_json.py" '{"default": "int"}' > "$TD_1"
+python3 $ANALYTICS_DIR"tsv_to_json.py" '{"default": "int"}' > "$TD_1"
 
 cat "$2" |
-python $ANALYTICS_DIR"tsv_to_json.py" '{"entity_num": "int"}' > "$TD_2"
+python3 $ANALYTICS_DIR"tsv_to_json.py" '{"entity_num": "int"}' > "$TD_2"
 
-python $ANALYTICS_DIR"make_audlist_dataset_json.py" "$TD_1" "$TD_2"
+python3 $ANALYTICS_DIR"make_audlist_dataset_json.py" "$TD_1" "$TD_2"
